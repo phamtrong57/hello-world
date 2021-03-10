d548436663b9a1933487997d5c3eeaa764bb6fab
phamtrong <phamtrong357@gmail.com>
2021-03-10 20:26:11 +0900

    keobuabaogame

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-a = "new articles"
-name = "KKa"
-print(3*a + name)
-
-
-aaaaaaaaa=""
+rule =[[0,2,1],[1,0,2],[2,1,0]]
+check=["hoa","thang","thua"]
+p1 = int(input("p1:= "))
+p2 = int(input("p2:= "))
+checkResult = rule[p1][p2]
+print(check[checkResult])
