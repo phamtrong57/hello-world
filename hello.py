e834a52222cc1ebd67f99f6037eec9f3b5277a95
phamtrong <phamdinhtrong357@gmail.com>
2021-03-26 00:01:19 +0900

    add keo bua bao code

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-a = "new articles"
-name = "KKa"
-print(3*a + name)
-
-
-aaaaaaaaa=""
+import random
+arr =[[0,1,2],[1,0,2],[2,1,0]]
+choice = ["keo","bua","bao"]
+num = random.randint(0,2)
+print(num)
